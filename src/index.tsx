cb052d6b52ecd2ecfb3369172135c9ecd84d9455
LesOne7 <55252052+LesOne7@users.noreply.github.com>
2020-05-14 12:46:03 +0300

    8 step

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks'; 
 import React from 'react';
+import gql from 'graphql-tag';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login';
+import { resolvers, typeDefs } from './resolvers';
 import injectStyles from './styles';
 
 const cache = new InMemoryCache();
@@ -20,6 +23,8 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
             authorization: localStorage.getItem('token'),
         },
     }),
+    typeDefs,
+    resolvers,
 });
 
 cache.writeData({
@@ -28,10 +33,22 @@ cache.writeData({
         cartItems: [],
     },
 });
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+    const { data } = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 injectStyles();
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <Pages />
+        <IsLoggedIn />
     </ApolloProvider>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
