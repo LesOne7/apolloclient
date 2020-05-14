0cffdb9ffdbef185b2938ed51d52c465c5483db4
LesOne7 <55252052+LesOne7@users.noreply.github.com>
2020-05-14 13:07:49 +0300

    changed src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,8 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
         uri: 'http://localhost:4000/graphql',
         headers: {
             authorization: localStorage.getItem('token'),
+            'client-name': 'Space Explorer [web]',
+            'client-version': '1.0.0',
         },
     }),
     typeDefs,
